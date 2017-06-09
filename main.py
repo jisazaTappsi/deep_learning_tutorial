fae26be10fa8c3d7697923f9751cdbeb75400cdb
Juan Pablo <juan.isaza@voiq.com>
2017-06-08 21:00:40 -0500

    adds comments

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Python3: Deep learning applied to sklearn small dataset
+
+
+To run first install Keras
+pip3 install keras
+
+And then install Tensorflow or Theano.
+
+To run do
+python3 main.py
+
+"""
+
 import numpy as np
 from sklearn import datasets
 from keras.models import Sequential
